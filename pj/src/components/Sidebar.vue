4b50996216a6507ef09fbc3c068965c7e0505680
Naoki Tsunoda <Tn04240709@icloud.com>
2021-04-28 08:58:31 +0900

    [add]サイドバーの外観作成完了

--- pj/src/components/Sidebar.vue
+++ pj/src/components/Sidebar.vue
@@ -1,9 +1,81 @@
 <template>
   <div>
-      <div>チーム名</div>
-      <div>タスク管理</div>
-      <div>マスタメンテ</div>
-      <div>チーム名</div>
-      <div>チーム名</div>
+      <div class="sidebar-header">チーム名</div>
+      <div class="sidebar-main">
+        <div>・アイウエオ1</div>
+        <div>・アイウエオ2</div>
+        <div>・アイウエオ3</div>
+        <div>・アイウエオ4</div>
+        <div>・アイウエオ5</div>
+        <div>・アイウエオ6</div>
+        <div>・アイウエオ7</div>
+        <div>・アイウエオ8</div>
+        <div>・アイウエオ9</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+        <div>・アイウエオ</div>
+      </div>
+
   </div>
 </template>
+
+<style>
+.sidebar-header{
+  background: rgb(123, 183, 201);
+  /* display: flex;
+  padding: 60px 20px; */
+  position: fixed;
+  /* justify-content: space-between; */
+  width: 325px;
+  height: 150px;
+}
+.sidebar-main {
+  margin-top: 150px;
+  background: rgb(173, 233, 188);
+}
+</style>
